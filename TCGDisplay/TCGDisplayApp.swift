64a9dde3a5c72c33efe2dc5a2c286b873531ce15
Filipe António <filipe.aa1995@gmail.com>
2026-01-28 01:48:46 +0000

    Initial Commit

---
+++ TCGDisplay/TCGDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCGDisplayApp.swift
+//  TCGDisplay
+//
+//  Created by Filipe de Almeida António on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TCGDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCGDisplayTests/TCGDisplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCGDisplayTests.swift
+//  TCGDisplayTests
+//
+//  Created by Filipe de Almeida António on 28/01/2026.
+//
+
+import Testing
+@testable import TCGDisplay
+
+struct TCGDisplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
